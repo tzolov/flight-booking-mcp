fb11123fe1c9119f02a5c657f103eaffcb97fe68
Christian Tzolov <christian.tzolov@broadcom.com>
2025-11-14 12:53:32 +0100

    upgrade to spring ai 1.1.0
    Signed-off-by: Christian Tzolov <christian.tzolov@broadcom.com>

--- flight-mcp-client/src/main/java/com/vaadin/lab/AiApplication.java
+++ flight-mcp-client/src/main/java/com/vaadin/lab/AiApplication.java
@@ -2,19 +2,13 @@ package com.vaadin.lab;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.lab.ai.McpClientHandlers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Push
 @SpringBootApplication
 public class AiApplication implements AppShellConfigurator {
-
-	// Workaround until https://github.com/spring-projects/spring-ai/pull/4671
-	@Autowired
-	private McpClientHandlers mcpClientHandlers; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiApplication.class, args);
